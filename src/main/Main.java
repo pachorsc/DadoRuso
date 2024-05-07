6f9efe7944390c942a53810c0872b9a7db1d9291
Carlos Madrid <37978151+Nozferatu@users.noreply.github.com>
2024-05-07 10:01:39 +0200

    Add files via upload

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,24 @@ import flojerasdependency.FlojerasUtility;
 
 public class Main extends FlojerasUtility {
 
+    public static Objeto objetoAUsar;
+    
     public static void main(String[] args) {
-        //Acciones dado
-        //1-4 Jugar a secas
-        //5-6 Obtienes un objeto
+        //Mínimo recomendado de vidas = 10
+    }
+    
+    public static Objeto generarObjeto(){
+        Objeto o;
+        int random = randomInt(1, 100);
+        
+        if(random >= 1 && random <= 45){
+            o = new Objeto(TipoObjeto.AGUACATE);
+        }else if(random >= 46 && random <= 89){
+            o = new Objeto(TipoObjeto.DARDO);
+        }else{
+            o = new Objeto(TipoObjeto.DARDO_DORADO);
+        }
+        
+        return o;
     }
 }
