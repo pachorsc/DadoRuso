9255339d953a4eb6bf08de4ef92c4598cf0ac2d8
pachorsc <93031142+pachorsc@users.noreply.github.com>
2024-05-07 11:47:30 +0200

    Usar objetos

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,7 @@ import flojerasdependency.FlojerasUtility;
 
 public class Main extends FlojerasUtility {
 
-    public static Objeto objetoAUsar;
+    public static Objeto objetoAUsar = null;
     public static final int VIDAS = 9;
     
     public static void main(String[] args) {
@@ -39,11 +39,13 @@ public class Main extends FlojerasUtility {
         boolean ganar = true;
         while(ganar){
             
+            objetoAUsar = null;
+            
             int turnj1 = J1.tirarDado();
             int turnbot = bot.tirarDado();
             System.out.println("Tu dado ha sacado un "+turnj1+" El dado de " + bot.getNombre() + " ha sacado un "+turnbot);
             
-            if(turnj1>turnbot){
+            if(turnj1>turnbot){     
                 //Turno jugador
                 System.out.println("Has sacado más que el bot, tienes el turno");
                 
@@ -54,7 +56,10 @@ public class Main extends FlojerasUtility {
                 
                 if (devolverNumEspaciosOcupados(J1.getInventario()) > 0) {
                     J1.turno(true);
+                    usarobj(objetoAUsar,J1,bot);
                 } else J1.turno(false);
+                
+                bot.dsiminuirVida();
                 
                 
                 
@@ -67,7 +72,10 @@ public class Main extends FlojerasUtility {
                 
                 if (devolverNumEspaciosOcupados(J1.getInventario()) > 0) {
                     bot.turno(true);
+                    usarobj(objetoAUsar,bot,J1);
                 } else bot.turno(false);
+                
+                J1.dsiminuirVida();
                 
             }
             System.out.println("Ronda terminada - Presione Enter para pasar a la siguiente");
@@ -89,9 +97,16 @@ public class Main extends FlojerasUtility {
         
         return o;
     }
-    public void usarobj(Objeto x, Jugador y){
-        if (x.getNombre() == TipoObjeto.AGUACATE) {
-            y.a
+    public static void usarobj(Objeto x, Jugador y, Jugador z){
+        if (x == null) {
+            
+        } else if (x.getNombre() == TipoObjeto.AGUACATE) {
+            y.aumentarVida();
+        } else if (x.getNombre() == TipoObjeto.DARDO) {
+            z.dsiminuirVida();
+        } else if (x.getNombre() == TipoObjeto.DARDO_DORADO) {
+            z.dsiminuirVida();
+            z.dsiminuirVida();
         }
     }
 }
